e70e735a1d42c33afa87328082dcd078feb20a57
Coda Hale <coda.hale@gmail.com>
2019-06-25 10:59:22 -0700

    Add tests for setup.

---
+++ pkg/belvedere/internal/setup/apis_test.go
@@ -0,0 +1,37 @@
+package setup
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/codahale/belvedere/pkg/belvedere/internal/it"
+	"github.com/codahale/belvedere/pkg/belvedere/internal/waiter"
+	"google.golang.org/api/serviceusage/v1"
+	"gopkg.in/h2non/gock.v1"
+)
+
+func TestEnableAPIs(t *testing.T) {
+	defer gock.Off()
+	it.MockTokenSource()
+
+	gock.New("https://serviceusage.googleapis.com/v1/projects/my-project/services:batchEnable?alt=json&prettyPrint=false").
+		JSON(serviceusage.BatchEnableServicesRequest{
+			ServiceIds: requiredServices,
+		}).
+		Reply(200).
+		JSON(serviceusage.Operation{
+			Name: "op1",
+		})
+
+	gock.New("https://serviceusage.googleapis.com/v1/op1?alt=json&fields=status%2Cerror&prettyPrint=false").
+		Reply(200).
+		JSON(serviceusage.Operation{
+			Done: true,
+		})
+
+	ctx := waiter.WithInterval(context.TODO(), 10*time.Millisecond)
+	if err := EnableAPIs(ctx, "my-project", false); err != nil {
+		t.Fatal(err)
+	}
+}
---
+++ pkg/belvedere/internal/setup/perms_test.go
@@ -0,0 +1,59 @@
+package setup
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/codahale/belvedere/pkg/belvedere/internal/it"
+	"github.com/codahale/belvedere/pkg/belvedere/internal/waiter"
+	"google.golang.org/api/cloudresourcemanager/v1"
+	"gopkg.in/h2non/gock.v1"
+)
+
+func TestSetDMPerms(t *testing.T) {
+	defer gock.Off()
+	it.MockTokenSource()
+
+	gock.New("https://cloudresourcemanager.googleapis.com/v1/projects/my-project?alt=json&fields=projectNumber&prettyPrint=false").
+		Reply(200).
+		JSON(cloudresourcemanager.Project{
+			ProjectNumber: 123456,
+		})
+
+	gock.New("https://cloudresourcemanager.googleapis.com/v1/projects/my-project:getIamPolicy?alt=json&prettyPrint=false").
+		Reply(200).
+		JSON(cloudresourcemanager.Policy{
+			Bindings: []*cloudresourcemanager.Binding{
+				{
+					Members: []string{"email:existing@example.com"},
+					Role:    "roles/passerby",
+				},
+			},
+			Etag: "300",
+		})
+
+	gock.New("https://cloudresourcemanager.googleapis.com/v1/projects/my-project:setIamPolicy?alt=json&prettyPrint=false").
+		JSON(cloudresourcemanager.SetIamPolicyRequest{
+			Policy: &cloudresourcemanager.Policy{
+				Bindings: []*cloudresourcemanager.Binding{
+					{
+						Members: []string{"email:existing@example.com"},
+						Role:    "roles/passerby",
+					},
+					{
+						Members: []string{"serviceAccount:123456@cloudservices.gserviceaccount.com"},
+						Role:    "roles/owner",
+					},
+				},
+				Etag: "300",
+			},
+		}).
+		Reply(200).
+		JSON(cloudresourcemanager.Policy{})
+
+	ctx := waiter.WithInterval(context.TODO(), 10*time.Millisecond)
+	if err := SetDMPerms(ctx, "my-project", false); err != nil {
+		t.Fatal(err)
+	}
+}
