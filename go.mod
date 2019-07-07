7f692c6a7270f8e839d065a713a3c55d1bb99864
Coda Hale <coda.hale@gmail.com>
2019-07-07 16:47:31 -0700

    Write CSV to STDOUT when it's not a terminal.

--- go.mod
+++ go.mod
@@ -14,7 +14,7 @@ require (
 	go.opencensus.io v0.22.0
 	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
-	golang.org/x/sys v0.0.0-20190626221950-04f50cda93cb // indirect
+	golang.org/x/sys v0.0.0-20190626221950-04f50cda93cb
 	golang.org/x/xerrors v0.0.0-20190513163551-3ee3066db522
 	google.golang.org/api v0.7.0
 	google.golang.org/genproto v0.0.0-20190701230453-710ae3a149df // indirect
--- output.go
+++ output.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"bytes"
+	"encoding/csv"
+	"os"
 
 	"github.com/olekukonko/tablewriter"
+	"golang.org/x/sys/unix"
 )
+
+func isTerminal() bool {
+	_, err := unix.IoctlGetTermios(int(os.Stdout.Fd()), unix.TIOCGETA)
+	return err == nil
+}
 
 func formatTable(headers []string, rows [][]string) string {
 	b := bytes.NewBuffer(nil)
-	tw := tablewriter.NewWriter(b)
-	tw.SetAutoFormatHeaders(false)
-	tw.SetHeader(headers)
-	tw.AppendBulk(rows)
-	tw.Render()
+	if isTerminal() {
+		tw := tablewriter.NewWriter(b)
+		tw.SetAutoFormatHeaders(false)
+		tw.SetHeader(headers)
+		tw.AppendBulk(rows)
+		tw.Render()
+	} else {
+		cw := csv.NewWriter(b)
+		_ = cw.Write(headers)
+		_ = cw.WriteAll(rows)
+		cw.Flush()
+	}
 	return b.String()
 }
