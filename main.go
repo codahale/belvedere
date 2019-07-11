4084a74c64459c5ac9e2074d97ad4a57fbb8ee6d
Coda Hale <coda.hale@gmail.com>
2019-07-11 12:17:12 -0700

    Document vars.

--- main.go
+++ main.go
@@ -17,7 +17,9 @@ import (
 )
 
 var (
-	version  = "unknown"
+	version = "unknown" // version is injected via the go:generate statement
+
+	// Application and common flags.
 	app      = kingpin.New("belvedere", "A fine place from which to survey your estate.")
 	debug    = app.Flag("debug", "Enable debug logging.").Bool()
 	quiet    = app.Flag("quiet", "Disable all logging.").Short('q').Bool()
@@ -29,26 +31,33 @@ var (
 		Default("10m").Duration()
 	project = app.Flag("project", "Specify a GCP project ID. Defaults to using gcloud.").String()
 
+	// belvedere setup <dns-zone>
 	setupCmd     = app.Command("setup", "Initialize a GCP project for use with Belvedere.")
 	setupDnsZone = setupCmd.Arg("dns-zone", "The DNS zone to be managed by this project.").
 			Required().String()
 
+	// belvedere teardown [--async]
 	teardownCmd   = app.Command("teardown", "Remove all Belvedere resources from this project.")
 	teardownAsync = teardownCmd.Flag("async", "Return without waiting for successful completion.").Bool()
 
+	// belvedere dns-servers
 	dnsServersCmd = app.Command("dns-servers", "List the DNS servers for this project.")
 
+	// belvedere machine-types
 	machineTypesCmd    = app.Command("machine-types", "List available GCE machine types.")
 	machineTypesRegion = machineTypesCmd.Arg("region", "Limit types to those available in the given region.").String()
 
+	// belvedere instances [<app>] [<release>]
 	instancesCmd     = app.Command("instances", "List running instances.")
 	instancesApp     = instancesCmd.Arg("app", "Limit instances to those running the given app.").String()
 	instancesRelease = instancesCmd.Arg("release", "Limit instances to those running the given release.").String()
 
+	// belvedere ssh <instance> [-- <args>...]
 	sshCmd      = app.Command("ssh", "SSH to an instance over IAP.")
-	sshInstance = sshCmd.Arg("instance", "The instance name.").String()
+	sshInstance = sshCmd.Arg("instance", "The instance name.").Required().String()
 	sshArgs     = sshCmd.Arg("args", "Additional SSH arguments.").Strings()
 
+	// belvedere logs <app> [<release>] [<instance>] [--filter=FILTER...] [--freshness=5m]
 	logsCmd       = app.Command("logs", "Display application logs.")
 	logsApp       = logsCmd.Arg("app", "Limit logs to the given app.").Required().String()
 	logsRelease   = logsCmd.Arg("release", "Limit logs to the given release.").String()
@@ -57,28 +66,36 @@ var (
 	logsFreshness = logsCmd.Flag("freshness", "Limit logs to the last period of time.").
 			Default("5m").Duration()
 
+	// belvedere apps
 	appsCmd = app.Command("apps", "Commands for managing apps.")
 
+	// belvedere apps list
 	appsListCmd = appsCmd.Command("list", "List all apps.")
 
+	// belvedere apps create <app> <region> <config>
 	appsCreateCmd    = appsCmd.Command("create", "Create an application.")
 	appsCreateApp    = appsCreateCmd.Arg("app", "The app's name.").Required().String()
 	appsCreateRegion = appsCreateCmd.Arg("region", "The app's region.").Required().String()
 	appsCreateConfig = appsCreateCmd.Arg("config", "The app's configuration.").Required().String()
 
+	// belvedere apps update <app> <config>
 	appsUpdateCmd    = appsCmd.Command("update", "Update an application.")
 	appsUpdateApp    = appsUpdateCmd.Arg("app", "The app's name.").Required().String()
 	appsUpdateConfig = appsUpdateCmd.Arg("config", "The app's configuration.").Required().String()
 
+	// belvedere apps delete <app> [--async]
 	appsDeleteCmd   = appsCmd.Command("delete", "Delete an application.")
 	appsDeleteApp   = appsDeleteCmd.Arg("app", "The app's name.").Required().String()
 	appsDeleteAsync = appsDeleteCmd.Flag("async", "Return without waiting for successful completion.").Bool()
 
+	// belvedere releases
 	relCmd = app.Command("releases", "Commands for managing releases.")
 
+	// belvedere releases list [<app>]
 	relListCmd = relCmd.Command("list", "List all releases.")
 	relListApp = relListCmd.Arg("app", "Limit releases to the given app.").String()
 
+	// belvedere releases create <app> <release> <config> <sha256> [--enable]
 	relCreateCmd     = relCmd.Command("create", "Create a release.")
 	relCreateApp     = relCreateCmd.Arg("app", "The app's name.").Required().String()
 	relCreateRelease = relCreateCmd.Arg("release", "The release's name.").Required().String()
@@ -86,14 +103,17 @@ var (
 	relCreateHash    = relCreateCmd.Arg("sha256", "The app container's SHA256 hash.").Required().String()
 	relCreateEnable  = relCreateCmd.Flag("enable", "Put release into service once created.").Bool()
 
+	// belvedere releases enable <app> <release>
 	relEnableCmd     = relCmd.Command("enable", "Put a release into service.")
 	relEnableApp     = relEnableCmd.Arg("app", "The app's name.").Required().String()
 	relEnableRelease = relEnableCmd.Arg("release", "The release's name.").Required().String()
 
+	// belvedere releases disable <app> <release>
 	relDisableCmd     = relCmd.Command("disable", "Remove a release from service.")
 	relDisableApp     = relDisableCmd.Arg("app", "The app's name.").Required().String()
 	relDisableRelease = relDisableCmd.Arg("release", "The release's name.").Required().String()
 
+	// belvedere releases delete <app> <release> [--async]
 	relDeleteCmd     = relCmd.Command("delete", "Delete a release.")
 	relDeleteApp     = relDeleteCmd.Arg("app", "The app's name.").Required().String()
 	relDeleteRelease = relDeleteCmd.Arg("release", "The releases's name.").Required().String()
