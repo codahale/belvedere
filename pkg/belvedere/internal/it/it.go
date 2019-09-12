96233df368a44a1288611b4c9ec3323195207528
Coda Hale <coda.hale@gmail.com>
2019-09-12 08:39:39 -0700

    Fix token source mocking.

--- pkg/belvedere/internal/it/it.go
+++ pkg/belvedere/internal/it/it.go
@@ -11,12 +11,19 @@ import (
 // MockTokenSource mocks the GCP SDK's default token source endpoint with a stub token.
 func MockTokenSource() {
 	_ = os.Setenv("GCE_METADATA_HOST", "metadata.server.fake")
+	token := oauth2.Token{
+		TokenType:    "mock",
+		AccessToken:  "access_token",
+		RefreshToken: "refresh_token",
+		Expiry:       time.Now().Add(10 * time.Hour),
+	}
+	// Mock out token source for default app credentials.
 	gock.New("https://oauth2.googleapis/token").
 		Reply(200).
-		JSON(oauth2.Token{
-			TokenType:    "mock",
-			AccessToken:  "access_token",
-			RefreshToken: "refresh_token",
-			Expiry:       time.Now().Add(10 * time.Hour),
-		})
+		JSON(token)
+
+	// Mock out token source for fake GCE tests.
+	gock.New("http://metadata.server.fake/computeMetadata/v1/instance/service-accounts/default/token").
+		Reply(200).
+		JSON(token)
 }
