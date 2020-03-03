e69a1af6ca151e2176dd3a78532d1eae69a510bc
Coda Hale <coda.hale@gmail.com>
2020-03-03 10:18:25 -0700

    Add tests for all release commands.

--- releases_test.go
+++ releases_test.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/codahale/belvedere/pkg/belvedere"
+	"github.com/codahale/belvedere/pkg/belvedere/cfg"
 	"github.com/golang/mock/gomock"
 )
 
@@ -37,6 +40,164 @@ func TestReleasesListCmd_Run(t *testing.T) {
 		Print(rels)
 
 	if err := cmd.Run(context.TODO(), project, tables); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestReleasesCreateCmd_Run(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	sha := strings.Repeat("a", 64)
+	cmd := &ReleasesCreateCmd{
+		App:           "my-app",
+		Release:       "v1",
+		SHA256:        sha,
+		Config:        FileContentFlag(`{"numReplicas":100}`),
+		Enable:        false,
+		ModifyOptions: ModifyOptions{},
+		LongRunningOptions: LongRunningOptions{
+			Interval: 100 * time.Millisecond,
+		},
+	}
+
+	config := &cfg.Config{
+		NumReplicas: 100,
+	}
+
+	releases := NewMockReleaseService(ctrl)
+	releases.EXPECT().
+		Create(gomock.Any(), "my-app", "v1", config, sha, false, 100*time.Millisecond)
+
+	project := NewMockProject(ctrl)
+	project.EXPECT().
+		Releases().
+		Return(releases)
+
+	if err := cmd.Run(context.TODO(), project); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestReleasesCreateCmd_Run_Enable(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	sha := strings.Repeat("a", 64)
+	cmd := &ReleasesCreateCmd{
+		App:           "my-app",
+		Release:       "v1",
+		SHA256:        sha,
+		Config:        FileContentFlag(`{"numReplicas":100}`),
+		Enable:        true,
+		ModifyOptions: ModifyOptions{},
+		LongRunningOptions: LongRunningOptions{
+			Interval: 100 * time.Millisecond,
+		},
+	}
+
+	config := &cfg.Config{
+		NumReplicas: 100,
+	}
+
+	releases := NewMockReleaseService(ctrl)
+	create := releases.EXPECT().
+		Create(gomock.Any(), "my-app", "v1", config, sha, false, 100*time.Millisecond)
+	releases.EXPECT().
+		Enable(gomock.Any(), "my-app", "v1", false, 100*time.Millisecond).
+		After(create)
+
+	project := NewMockProject(ctrl)
+	project.EXPECT().
+		Releases().
+		Return(releases).
+		AnyTimes()
+
+	if err := cmd.Run(context.TODO(), project); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestReleasesEnableCmd_Run(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	cmd := &ReleasesEnableCmd{
+		App:           "my-app",
+		Release:       "v1",
+		ModifyOptions: ModifyOptions{},
+		LongRunningOptions: LongRunningOptions{
+			Interval: 100 * time.Millisecond,
+		},
+	}
+
+	releases := NewMockReleaseService(ctrl)
+	releases.EXPECT().
+		Enable(gomock.Any(), "my-app", "v1", false, 100*time.Millisecond)
+
+	project := NewMockProject(ctrl)
+	project.EXPECT().
+		Releases().
+		Return(releases).
+		AnyTimes()
+
+	if err := cmd.Run(context.TODO(), project); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestReleasesDisableCmd_Run(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	cmd := &ReleasesDisableCmd{
+		App:           "my-app",
+		Release:       "v1",
+		ModifyOptions: ModifyOptions{},
+		LongRunningOptions: LongRunningOptions{
+			Interval: 100 * time.Millisecond,
+		},
+	}
+
+	releases := NewMockReleaseService(ctrl)
+	releases.EXPECT().
+		Disable(gomock.Any(), "my-app", "v1", false, 100*time.Millisecond)
+
+	project := NewMockProject(ctrl)
+	project.EXPECT().
+		Releases().
+		Return(releases).
+		AnyTimes()
+
+	if err := cmd.Run(context.TODO(), project); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestReleasesDeleteCmd_Run(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	cmd := &ReleasesDeleteCmd{
+		App:           "my-app",
+		Release:       "v1",
+		ModifyOptions: ModifyOptions{},
+		LongRunningOptions: LongRunningOptions{
+			Interval: 100 * time.Millisecond,
+		},
+	}
+
+	releases := NewMockReleaseService(ctrl)
+	releases.EXPECT().
+		Delete(gomock.Any(), "my-app", "v1", false, false, 100*time.Millisecond)
+
+	project := NewMockProject(ctrl)
+	project.EXPECT().
+		Releases().
+		Return(releases).
+		AnyTimes()
+
+	if err := cmd.Run(context.TODO(), project); err != nil {
 		t.Fatal(err)
 	}
 }
