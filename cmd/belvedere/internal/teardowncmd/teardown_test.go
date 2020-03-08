4de2f6bccd3961cd8624c00a49eca274643d3b15
Coda Hale <coda.hale@gmail.com>
2020-03-08 12:54:34 -0600

    Add tests for teardowncmd.

---
+++ cmd/belvedere/internal/teardowncmd/teardown_test.go
@@ -0,0 +1,27 @@
+package teardowncmd
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/codahale/belvedere/cmd/belvedere/internal/mocks"
+	"github.com/codahale/belvedere/cmd/belvedere/internal/rootcmd"
+	"github.com/golang/mock/gomock"
+)
+
+func TestNew(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	project := mocks.NewMockProject(ctrl)
+	project.EXPECT().
+		Teardown(gomock.Any(), false, false, 10*time.Second)
+
+	command := New(&rootcmd.Config{
+		Project: project,
+	})
+	if err := command.ParseAndRun(context.Background(), []string{}); err != nil {
+		t.Fatal(err)
+	}
+}
