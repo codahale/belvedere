297ee9024782ebc573a3db250cfa9d2c35b9c1fb
Coda Hale <coda.hale@gmail.com>
2020-03-08 13:15:11 -0600

    Add tests for secrets/createcmd and fix bug.

---
+++ cmd/belvedere/internal/secretscmd/createcmd/create_test.go
@@ -0,0 +1,75 @@
+package createcmd
+
+import (
+	"context"
+	"testing"
+
+	"github.com/codahale/belvedere/cmd/belvedere/internal/mocks"
+	"github.com/codahale/belvedere/cmd/belvedere/internal/rootcmd"
+	"github.com/golang/mock/gomock"
+)
+
+func TestSecretsCreate(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	files := mocks.NewMockFileReader(ctrl)
+	files.EXPECT().
+		Read("-").
+		Return([]byte(`value`), nil)
+
+	secrets := mocks.NewMockSecretsService(ctrl)
+	secrets.EXPECT().
+		Create(gomock.Any(), "one", []byte(`value`), false)
+
+	project := mocks.NewMockProject(ctrl)
+	project.EXPECT().
+		Secrets().
+		Return(secrets).
+		AnyTimes()
+
+	command := New(&rootcmd.Config{
+		Project: project,
+		Files:   files,
+	})
+	if err := command.ParseAndRun(context.Background(),
+		[]string{
+			"one",
+		},
+	); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSecretsCreate_WithFile(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	files := mocks.NewMockFileReader(ctrl)
+	files.EXPECT().
+		Read("secret.txt").
+		Return([]byte(`value`), nil)
+
+	secrets := mocks.NewMockSecretsService(ctrl)
+	secrets.EXPECT().
+		Create(gomock.Any(), "one", []byte(`value`), false)
+
+	project := mocks.NewMockProject(ctrl)
+	project.EXPECT().
+		Secrets().
+		Return(secrets).
+		AnyTimes()
+
+	command := New(&rootcmd.Config{
+		Project: project,
+		Files:   files,
+	})
+	if err := command.ParseAndRun(context.Background(),
+		[]string{
+			"one",
+			"secret.txt",
+		},
+	); err != nil {
+		t.Fatal(err)
+	}
+}
