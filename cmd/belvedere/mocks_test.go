a6ceb626d820a7fa2d1c34a9205a84c460383761
Coda Hale <coda.hale@gmail.com>
2020-06-24 20:34:48 -0600

    Fix cmd mocks.

--- cmd/belvedere/mocks_test.go
+++ cmd/belvedere/mocks_test.go
@@ -1,8 +1,8 @@
 package main
 
-//go:generate mockgen -package main -destination mock_project.go -source ../../pkg/belvedere/belvedere.go Project
-//go:generate mockgen -package main -destination mock_secrets.go -source ../../pkg/belvedere/secrets.go SecretsService
-//go:generate mockgen -package main -destination mock_logs.go -source ../../pkg/belvedere/logs.go LogService
-//go:generate mockgen -package main -destination mock_output.go -source ./internal/cli/output.go Output
-//go:generate mockgen -package main -destination mock_apps.go -source ../../pkg/belvedere/apps.go AppService
-//go:generate mockgen -package main -destination mock_releases.go -source ../../pkg/belvedere/releases.go ReleaseService
+//go:generate mockgen -package main -destination mock_project_test.go -source ../../pkg/belvedere/belvedere.go Project
+//go:generate mockgen -package main -destination mock_secrets_test.go -source ../../pkg/belvedere/secrets.go SecretsService
+//go:generate mockgen -package main -destination mock_logs_test.go -source ../../pkg/belvedere/logs.go LogService
+//go:generate mockgen -package main -destination mock_output_test.go -source ./internal/cli/output.go Output
+//go:generate mockgen -package main -destination mock_apps_test.go -source ../../pkg/belvedere/apps.go AppService
+//go:generate mockgen -package main -destination mock_releases_test.go -source ../../pkg/belvedere/releases.go ReleaseService
