b1092a0df09a86f34768dbc37a323f778e27167e
Coda Hale <coda.hale@gmail.com>
2020-06-24 20:39:14 -0600

    Fix linter issues.

--- cmd/belvedere/mocks_test.go
+++ cmd/belvedere/mocks_test.go
@@ -1,5 +1,6 @@
 package main
 
+//nolint:lll // no way to make these shorter
 //go:generate mockgen -package main -destination mock_project_test.go -source ../../pkg/belvedere/belvedere.go Project
 //go:generate mockgen -package main -destination mock_secrets_test.go -source ../../pkg/belvedere/secrets.go SecretsService
 //go:generate mockgen -package main -destination mock_logs_test.go -source ../../pkg/belvedere/logs.go LogService
