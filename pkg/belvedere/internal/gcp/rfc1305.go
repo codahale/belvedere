f24365f44e42aae90b09cb3ed0ee2f786b44b4db
Coda Hale <coda.hale@gmail.com>
2020-06-27 11:15:00 -0600

    Stop exporting another internal error.

--- pkg/belvedere/internal/gcp/rfc1305.go
+++ pkg/belvedere/internal/gcp/rfc1305.go
@@ -10,18 +10,18 @@ var (
 	rfc1035 = regexp.MustCompile(`^[a-z]([-a-z0-9]*[a-z0-9])?$`)
 )
 
-type InvalidNameError struct {
+type invalidNameError struct {
 	Name string
 }
 
-func (e *InvalidNameError) Error() string {
+func (e *invalidNameError) Error() string {
 	return fmt.Sprintf("invalid name: %q", e.Name)
 }
 
 // ValidateRFC1035 returns an error if the given name is not a valid RFC1035 DNS name.
 func ValidateRFC1035(name string) error {
 	if len(name) < 1 || len(name) > 63 || !rfc1035.MatchString(name) {
-		return &InvalidNameError{Name: name}
+		return &invalidNameError{Name: name}
 	}
 
 	return nil
